cda97e2f7301c584e4786b27d1c418475fca9c01
tanimutomo <tanimutomo@gmail.com>
2020-02-19 19:39:13 +0900

    Add tutorial template pages

---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import './main.scss'
+import Nav from "./Nav";
+
+const Layout = ({ children }) => (
+  <div className="layout">
+    <Nav />
+    <main className="main">{ children }</main>
+  </div>
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Layout;
