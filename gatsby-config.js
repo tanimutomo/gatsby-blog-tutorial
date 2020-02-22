a5311c717ade6260ba2561eb1e85bd73be5f71bb
tanimutomo <tanimutomo@gmail.com>
2020-02-22 12:31:24 +0900

    Add gatsby-transformer-remark package

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
